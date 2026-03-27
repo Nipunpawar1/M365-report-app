c2b1cc2b5970c89e9be51fbe7dafb467fa8dd91e
Nipunpawar1 <62242802+Nipunpawar1@users.noreply.github.com>
2026-03-27 21:15:31 +1000

    Update msal-config.js

--- msal-config.js
+++ msal-config.js
@@ -1,10 +1,10 @@
 const msalConfig = {
     auth: {
-        clientId: "078e3278-8e1c-472e-88d7-633d52b56a2e
-",
+        clientId: "078e3278-8e1c-472e-88d7-633d52b56a2e",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: window.location.origin
+        redirectUri: "https://nipunpawar1.github.io/M365-report-app"
     }
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
+``
